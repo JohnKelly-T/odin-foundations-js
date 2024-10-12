0c2bf9e440f5b1dc163d405762100ab43fa58dbf
john-kelly <johnkellytemena@gmail.com>
2024-10-12 19:23:50 +0800

    Move activity files to subfolder

---
+++ variables-and-operators/script.js
@@ -0,0 +1,45 @@
+console.log("Hello World!");
+
+// declare variables
+
+let name = "John Kelly";
+let surname = "Temeña";
+
+// output to console
+
+console.log(name);
+console.log(surname);
+
+// re-assigning variables
+let age = 11;
+console.log(age);
+
+age = 22;
+
+console.log(age);
+
+// Declare constant variable
+const pi = 3.14;
+
+console.log(pi); // will output Uncaught TypeError: Assignment to constant variable
+
+// Assignment
+
+// 1. Add 2 numbers together
+
+console.log(12 + 27);
+
+// 2. Add a sequence of 6 different numbers
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+
+// 3. Print the value of the following expression: (4 + 6 + 9) // 77
+console.log((4 + 6 + 9) / 77);
+
+// 4. Using Variables in the console (note: you can't declare variables in the console)
+let a = 10;
+console.log(a);
+
+// 5. Try sequence
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
