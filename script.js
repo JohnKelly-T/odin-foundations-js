d784ecea6855a42130c5723f0ad67f615d3de7e0
john-kelly <johnkellytemena@gmail.com>
2024-10-12 19:06:20 +0800

    Reassign variables

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 console.log("Hello World!");
 
+// declare variables
+
 let name = "John Kelly";
 let surname = "Temeña";
 
+// output to console
+
 console.log(name);
 console.log(surname);
+
+// re-assigning variables
+let age = 11;
+console.log(age);
+
+age = 22;
+
+console.log(age);
